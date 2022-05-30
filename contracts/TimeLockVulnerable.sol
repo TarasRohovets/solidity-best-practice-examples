186993d6600a465ba8f5e35ab246c5b43569b5b2
Taras <rohovets.taras@gmail.com>
2022-05-30 22:23:47 +0100

    init

---
+++ contracts/TimeLockVulnerable.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.8.14;
+
+contract TimeLockVulnerable {
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public lockTime;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        lockTime[msg.sender] = block.timestamp + 1 weeks;
+    }
+
+    function increaseLockTime(uint256 _secondsToIncrease) public {
+        lockTime[msg.sender] += _secondsToIncrease;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0, "where is lambo");
+        require(block.timestamp > lockTime[msg.sender], "too early my friend");
+
+        uint256 balance = balances[msg.sender];
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(balance);
+    }
+}
